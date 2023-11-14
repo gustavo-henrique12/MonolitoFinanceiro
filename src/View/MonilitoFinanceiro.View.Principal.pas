4a14bd5a9c1b73da9793a0d85e646a76c963b867
Gustavo Santos <gustavodossantos764@gmail.com>
2023-11-13 20:28:55 -0400

    estruturadas pastas do projeto e tela inicia do Sistema

---
+++ src/View/MonilitoFinanceiro.View.Principal.pas
@@ -0,0 +1,28 @@
+unit MonilitoFinanceiro.View.Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TFrmPrincipal = class(TForm)
+    MainMenu1: TMainMenu;
+    mnuCadastro: TMenuItem;
+    mnuRelatorios: TMenuItem;
+    mnuAjuda: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmPrincipal: TFrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
